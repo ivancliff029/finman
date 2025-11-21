b6f52796662a11d01271b961b31ba24392403e4e
ivancliff029 <odekeivancliff@gmail.com>
2025-11-21 10:50:46 +0300

    Update:simple landing page

---
+++ src/components/Landing/OfficialLanding.jsx
@@ -0,0 +1,10 @@
+export default function OfficialLanding() {
+    return (
+        <>
+            <div>
+                <h1>Welcome to the Official Landing Page</h1>
+                <p>This is the official landing page of our application.</p>
+            </div>
+        </>
+    )
+}
